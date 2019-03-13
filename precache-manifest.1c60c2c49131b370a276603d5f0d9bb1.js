d75b8a6e0b8655f3346f80f4e5eafb5979715fb8
Andrey Pitunov <andreypitunov@gmail.com>
2019-03-13 17:24:14 +0300

    Updates

---
+++ precache-manifest.1c60c2c49131b370a276603d5f0d9bb1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "255df1e50e6a7791b334cbf20a1a95d3",
+    "url": "/static/media/promo3.255df1e5.jpg"
+  },
+  {
+    "revision": "54c401b334562776b256dda41176b2ab",
+    "url": "/static/media/promo1.54c401b3.jpg"
+  },
+  {
+    "revision": "0771ae894fd66af874f3ec7e4ac374d9",
+    "url": "/static/media/promo.0771ae89.jpg"
+  },
+  {
+    "revision": "f5a15d5bd4e24c49a013afa90133703c",
+    "url": "/static/media/contact.f5a15d5b.jpg"
+  },
+  {
+    "revision": "67d640de8da6c6cd2422f7e911bfd5d2",
+    "url": "/static/media/block.67d640de.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1e88e726a2480f91ed59",
+    "url": "/static/js/main.1e88e726.chunk.js"
+  },
+  {
+    "revision": "1723b0c6a02cd2daf2b3",
+    "url": "/static/js/2.1723b0c6.chunk.js"
+  },
+  {
+    "revision": "a9e336ea3f68afa3efcb3d325ffabffc",
+    "url": "/index.html"
+  }
+];
